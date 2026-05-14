5d56f5f37306a72d96fefc017c5f0f7cb467091f
Gabriel Blanchet-Poitras <gabrielbpoitras@gmail.com>
2026-05-14 16:16:41 -0400

    Added depth shadows to the carousel components . Made a new circle variant .

--- app/carousel/page.tsx
+++ app/carousel/page.tsx
@@ -1,4 +1,5 @@
 import CarouselArc from "../components/carousel/arc"
+import Spacer from "../components/spacer"
 
 const images = ['tall0.jpg', 'tall1.jpg', 'tall3.jpg' , 
     'tall3.jpg' , 'tall3.jpg']
@@ -6,7 +7,9 @@ const images = ['tall0.jpg', 'tall1.jpg', 'tall3.jpg' ,
 export default function Carousel(){
     return (
         <>
-        <CarouselArc images={images}/>
+         <Spacer h={20}/>
+        <CarouselArc images={images} style="circle"/>
+          <Spacer h={20}/>
         </>
     )
 }
--- app/components/carousel/arc.tsx
+++ app/components/carousel/arc.tsx
@@ -5,57 +5,61 @@ import { List } from "lodash"
 import { useEffect, useState, useRef, MouseEvent } from "react"
 
 type CarArc = {
-    images : string[]
+    images : string[], style: string
 }
 type mappingObject = {
     left: string, right:string
 }
 
-export default function CarouselArc({images}:CarArc){
+export default function CarouselArc({images, style}:CarArc){
 
     const current_idx = useRef(2)
     const transition_list = useRef<{left:string, right:string}[]>([])
   
     const [direction, setDirection] = useState<null | 'left' | 'right'>(null)
 
-    const timeoutRef = useRef<number | null>(0)
+  
     const [init, setInit] = useState(false)
     const [transition_mappings, setTransi] = useState<Record<number, mappingObject>>({})
     
     useEffect(()=> {
 
-  
+    if (style && style == 'arc'){
     transition_list.current = [{left: 'left_to_right_t', right: 'right_to_left_t'}, 
                                        {left: 't_1_0', right: 't_0_1'},
                                        {left: 't_2_1', right: 't_1_2'},
                                        {left: 't_3_2', right: 't_2_3'},
                                        {left: 't_4_3', right: 't_3_4'}]
-                                       
+    }
+    else if (style && style == 'circle'){
+        transition_list.current = [{left: 'left_to_right_c halt_rotation', right: 'right_to_left_c halt_rotation'}, 
+                                       {left: 'c_1_0', right: 'c_0_1'},
+                                       {left: 'c_2_1', right: 'c_1_2'},
+                                       {left: 'c_3_2', right: 'c_2_3'},
+                                       {left: 'c_4_3', right: 'c_3_4'}]
+    }
+
+
+
     const temp:Record<number, mappingObject> = {}     
 
       transition_list.current.map((t, idx) => {
                         temp[idx] = t})
     setTransi(temp)
-     return () => {
-        if (timeoutRef.current){
-           
-            window.clearTimeout(timeoutRef.current)
-            timeoutRef.current = null
-        }
-     }
+  
     }, [])
 
  
     function switchIdx(direction:string){
         
-        if (current_idx.current == images.length && direction == 'left'){
+        if (current_idx.current == images.length -1 && direction == 'left'){
             current_idx.current = 0
         }
-        else if (current_idx.current < images.length && direction == 'left'){
+        else if (current_idx.current < images.length -1 && direction == 'left'){
             current_idx.current += 1
         }
         else if (current_idx.current == 0 && direction == 'right'){
-           current_idx.current = images.length
+           current_idx.current = images.length - 1 
         }
         else{
             current_idx.current -= 1
@@ -102,6 +106,12 @@ export default function CarouselArc({images}:CarArc){
               setTransi(temp)
         
     }
+    function get_edges_idx(){ 
+        const c = current_idx.current 
+        const edges = c == 2? [0, 4] : c == 3? [1, 0]: c == 4? [2, 1]:  c == 1? [4, 3]: [3, 2]
+        return edges
+    }
+
 
     const ReorderImages = (e:MouseEvent) => {
              let direction_changed
@@ -150,24 +160,33 @@ export default function CarouselArc({images}:CarArc){
          }
 
     return (
-        <div className="row d-flex justify-content-center">
+        <div className="row d-flex justify-content-center overflow-y-hidden overflow-x-md-visible overflow-x-hidden">
       
-            <div className="col-md-6 col-11 car_arc_row position-relative">
+            <div className={`${style == 'circle'? 'col-md-8': 'col-md-6' } col-12 car_${style}_row position-relative`}>
               {images.map((i, idx)=>{
 
                 return (
                     <div key={idx} className={`${init && direction ? transition_mappings[idx][direction]: ''} 
-                                               ${`car_arc_cont_${idx}`}`}
+                                               ${`car_${style}_cont_${idx}`} 
+                                              `}
                  
                      onMouseUp={(e)=> ReorderImages(e)}
                
-                      style={{pointerEvents: current_idx.current == idx? 'none' : 'all',
+                      style={{pointerEvents: current_idx.current == idx ? 'none' : 'all',
                             
                               position: 'absolute',
-                              boxShadow: '6px 3px 20px black'
+                          
+                              backgroundColor: current_idx.current == idx ?'white' : 'rgb(0, 0, 0)',
+                              
+                              
                       }}>
 
-                          <img src={i} className={'arc_img'}/>
+                          <img src={i} className={`${style}_img`} 
+                          style={{opacity: current_idx.current == idx ? '1' 
+                                         : get_edges_idx().includes(idx)? '0.8':
+                                           
+                            '0.95'
+                                                                  }}/>
                     </div>
                 )
               })
